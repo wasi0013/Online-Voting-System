266f9ff45f9806df647df7b63570e8d24c30fbfa
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-10-26 01:42:44 +0600

    Better Formatting client.js

--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,10 @@
-console.log("Hello World! from client");
+
 Template.quickVote.helpers({ isAdmin: function (){
-return Meteor.user()? (Meteor.user().username==="admin"?true:false):false;
+
+    return Meteor.user()? (
+        Meteor.user().username==="admin"?
+                true:false
+        )
+    :false;
 
 }})
