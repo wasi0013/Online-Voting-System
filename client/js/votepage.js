5d7a0254d4cac0b171a03377104ad483a37b84a9
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-10-29 00:41:06 +0600

    silly fix on empty user reg

--- client/js/votepage.js
+++ client/js/votepage.js
@@ -4,7 +4,7 @@ Template.quickVote.events({
     'click #c-register': function (evt, template) {
         // ...username holds voterid
         usern = template.find("#c-username").value
-        if (Meteor.user().username ==usern && usern != undefined && quickvote.find({username:usern})?(quickvote.find({username:usern}).count()==0):true){
+        if (Meteor.user().username ==usern && usern != undefined && usern!="" && quickvote.find({username:usern})?(quickvote.find({username:usern}).count()==0):true){
 
             quickvote.insert({username:usern, votecount:0 });
             alert("You are now registered as a candidate!");
