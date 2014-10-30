f0ce33f3b3d6cc9c55483fa347a28eb008e721c9
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-10-30 19:06:53 +0600

    successful test of chart.js

--- client/timer/timer.js
+++ client/timer/timer.js
@@ -74,10 +74,47 @@ Template.timer.rendered = function(evt,template) {
       this._rendered = true;
       //todo get data from database and render the timer
       votes=Session.get("vote");
+
       
 
 
       countdown(year,month,day,hour,minute)
+      
+    }
+}
+
+
+
+Template.stats.rendered = function(evt,template) {
+    if(!this._rendered) {
+      this._rendered = true;
+      var data = {
+  labels : ["January","February","March","April","May","June","July"],
+  datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,1)",
+      pointColor : "rgba(220,220,220,1)",
+      pointStrokeColor : "#fff",
+      data : [65,59,90,81,56,55,40]
+    },
+    {
+      fillColor : "rgba(151,187,205,0.5)",
+      strokeColor : "rgba(151,187,205,1)",
+      pointColor : "rgba(151,187,205,1)",
+      pointStrokeColor : "#fff",
+      data : [28,48,40,19,96,27,100]
+    }
+  ]
+}
+
+ 
+$( document ).ready(function() {
+    //Get the context of the canvas element we want to select
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myNewChart = new Chart(ctx).Line(data,null);
+  });
+     
       
     }
 }
