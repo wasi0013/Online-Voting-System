494d0ed7d8a805c039c65031d3e8874e298b002d
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-10-30 11:56:10 +0600

    added log render

--- client/js/client.js
+++ client/js/client.js
@@ -8,3 +8,15 @@ Template.quickVote.helpers({ isAdmin: function (){
     :false;
 
 }})
+
+function logRenders () {
+    _.each(Template, function (template, name) {
+      var oldRender = template.rendered;
+      var counter = 0;
+ 
+      template.rendered = function () {
+        console.log(name, "render count: ", ++counter);
+        oldRender && oldRender.apply(this, arguments);
+      };
+    });
+  }
